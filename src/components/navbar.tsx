055c14a019aef3cf315a34e5b56facc621294176
Florian Frantzen <florian.frantzen@rwth-aachen.de>
2025-08-15 19:25:48 +0200

    Make navbar interactive on mobile
    See #17

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,8 +5,10 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import classnames from "classnames";
 
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLeaf } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faLeaf, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const links = [
   { name: "Home", link: "/" },
@@ -26,61 +28,22 @@ const Navbar: FC = () => {
   };
 
   return (
-    <nav className="sticky top-0 z-50 bg-white shadow-sm">
+    <Disclosure as="nav" className="relative sm:sticky top-0 z-50 bg-white shadow-sm">
       <div className="mx-auto max-w-7xl px-2 sm:px-8">
         <div className="relative flex h-16 justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <button
+            <DisclosureButton
               type="button"
-              className="focus:outline-hidden relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-inset focus:ring-white"
+              className="group focus:outline-hidden relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
-              {/*
-                Icon when menu is closed.
-
-                Menu open: "hidden", Menu closed: "block"
-            */}
-              <svg
-                className="block size-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-              {/*
-
-                Icon when menu is open.
-
-                Menu open: "block", Menu closed: "hidden"
-            */}
-              <svg
-                className="hidden size-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18 18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
+              <FontAwesomeIcon icon={faBars} aria-hidden="true" className="block! size-6 group-data-open:hidden!" />
+              <FontAwesomeIcon icon={faXmark} aria-hidden="true" className="hidden! size-6 group-data-open:block!" />
+            </DisclosureButton>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <Link
@@ -116,29 +79,32 @@ const Navbar: FC = () => {
       </div>
 
       {/* Mobile menu, show/hide based on menu state. */}
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {links.map((link) => {
-            const active = isActive(link.link);
-            return (
-              <Link
-                key={link.link}
-                href={link.link}
-                className={classnames(
-                  "block rounded-md px-3 py-2 text-base font-medium",
-                  {
-                    "bg-gray-900 text-white": active,
-                    "text-gray-300 hover:bg-gray-700 hover:text-white": !active,
-                  },
-                )}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
-        </div>
-      </div>
-    </nav>
+      <DisclosurePanel className="sm:hidden">
+        {({ close }) => (
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {links.map((link) => {
+              const active = isActive(link.link);
+              return (
+                <Link
+                  key={link.link}
+                  href={link.link}
+                  className={classnames(
+                    "block rounded-md px-3 py-2 text-base font-medium",
+                    {
+                      "bg-gray-900 text-white": active,
+                      "text-gray-300 hover:bg-gray-700 hover:text-white": !active,
+                    },
+                  )}
+                  onNavigate={() => close()}
+                >
+                  {link.name}
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </DisclosurePanel>
+    </Disclosure>
   );
 };
 
