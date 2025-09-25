299cea9f2d337ff944e1b4486179ac3f09152e1b
Florian Frantzen <florian.frantzen@rwth-aachen.de>
2025-09-25 18:14:16 +0200

    Upload `mathoverflow-answers` dataset

--- scripts/mathoverflow-answers.py
+++ scripts/mathoverflow-answers.py
@@ -13,11 +13,11 @@ from collections import Counter
 from itertools import chain
 from pathlib import Path
 
-import toponetx as tnx
 import yaml
 from more_itertools import first
 from rich.progress import track
 
+from .benson import load_benson_hyperedges
 from .utils.yaml import patch_dumper
 
 patch_dumper()
@@ -26,7 +26,7 @@ root_dir = Path(__file__).parent.parent
 dataset_file = root_dir / "public" / "datasets" / "mathoverflow-answers.txt.gz"
 datasheet_file = root_dir / "src" / "datasets" / "mathoverflow-answers.mdx"
 
-nodes, hyperedges = tnx.datasets.benson.load_benson_hyperedges(
+nodes, hyperedges = load_benson_hyperedges(
     root_dir / "data" / "mathoverflow-answers"
 )
 
