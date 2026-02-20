2eecdac77202386e84879de0a964ceb6950a90e2
Florian Frantzen <florian.frantzen@rwth-aachen.de>
2026-02-20 17:15:48 +0100

    Update `eslint` config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,21 @@
+import { defineConfig } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-config-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
+export default defineConfig([
   {
     ignores: [
-      "node_modules/**",
       ".next/**",
-      "out/**",
+      ".venv/**",
       "build/**",
-      "next-env.d.ts",
+      "node_modules/**",
+      "out/**",
       "public/_pagefind/**",
+      "next-env.d.ts",
     ],
   },
   ...nextCoreWebVitals,
   ...nextTypescript,
-  ...compat.extends("prettier"),
-];
-
-export default eslintConfig;
+  prettier,
+]);
