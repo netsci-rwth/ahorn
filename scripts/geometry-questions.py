d8259d258f0dd0f19e9f29fc8a583771edb16a8d
Florian Frantzen <florian.frantzen@rwth-aachen.de>
2026-02-25 13:32:57 +0100

    Second revision of `geometry-questions`
    - Rename attribute `genre` to `category`
    - gzip dataset file

--- scripts/geometry-questions.py
+++ scripts/geometry-questions.py
@@ -6,6 +6,7 @@ References
 https://www.cs.cornell.edu/~arb/data/cat-edge-geometry-questions/
 """
 
+import gzip
 from collections import Counter, defaultdict
 from itertools import chain
 from pathlib import Path
@@ -25,8 +26,9 @@ from .utils.yaml import patch_dumper
 patch_dumper()
 
 root_dir = Path(__file__).parent.parent
-dataset_file = root_dir / "public" / "datasets" / "geometry-questions.txt"
+dataset_file = root_dir / "public" / "datasets" / "geometry-questions.txt.gz"
 datasheet_file = root_dir / "src" / "datasets" / "geometry-questions.mdx"
+revision = 2
 
 nodes, hyperedges = load_benson_hyperedges(
     root_dir / "data" / "cat-edge-geometry-questions"
@@ -37,14 +39,14 @@ edge_degree_counts = defaultdict(int)
 
 # write dataset file
 covered_nodes = set(chain.from_iterable(hyperedge.elements for hyperedge in hyperedges))
-with dataset_file.open("w") as f:
-    write_dataset_metadata(f, datasheet_file.stem, revision=1)
+with gzip.open(dataset_file, "wt") as f:
+    write_dataset_metadata(f, datasheet_file.stem, revision=revision)
     for node in track(map(first, nodes), description="Writing nodes"):
         if node in covered_nodes:
             continue
         write_node(f, node)
     for hyperedge in track(hyperedges, description="Writing hyperedges"):
-        write_edge(f, hyperedge, genre=hyperedge["label"])
+        write_edge(f, hyperedge, category=hyperedge["label"])
 
         for node in hyperedge.elements:
             node_degrees[node] += 1
@@ -65,7 +67,7 @@ update_frontmatter(
     datasheet_file,
     {
         "attachments": {
-            "dataset": {
+            f"revision-{revision}": {
                 "url": dataset_file.name,
                 "size": dataset_file.stat().st_size,
             }
